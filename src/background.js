c054d3e88408dc0f340f6ee579d0301dc5fb1efb
hrysdev <hrysdev@gmail.com>
2024-05-04 23:59:22 +0900

    add: background.jsの機能追加

--- src/background.js
+++ src/background.js
@@ -9,17 +9,22 @@
 //   }
 // }
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (tab.url.indexOf("https://twitter.com/") > -1) {
-    // // ページのURLを取得
-    // const url = decodeURIComponent(tab.url);
-    // console.log(url);
-
-    // // URLからリザルト結果を抽出
-    // const score = url.match(/★(\d,\d+)/)[1];
-    // const speed = url.match(/速度：(\d+.\d+)/)[1];
-    // const typo = url.match(/ミス：(\d+)/)[1];
-    // const course = url.match(/(\d,\d+)円コース/)[1];
-    // console.log(score, speed, typo, course);
-  }
+chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+  console.log("hoge");
 });
+
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   console.log(tabId);
+//   console.log(changeInfo.status);
+//   if (tab.url.indexOf("https://twitter.com/") > -1) {
+//     let url = decodeURIComponent(tab.url);
+//     console.log(url);
+//   }
+//   // ページのURLを取得
+//   // // URLからリザルト結果を抽出
+//   // const score = url.match(/★(\d,\d+)/)[1];
+//   // const speed = url.match(/速度：(\d+.\d+)/)[1];
+//   // const typo = url.match(/ミス：(\d+)/)[1];
+//   // const course = url.match(/(\d,\d+)円コース/)[1];
+//   // console.log(score, speed, typo, course);
+// });
