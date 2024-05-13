b5380a868d7674c3683a883319b0b218ede9bd4d
hrysdev <hrysdev@gmail.com>
2024-05-13 20:43:15 +0900

    add: tsファイルを追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

---
+++ src/background.ts
@@ -0,0 +1,44 @@
+// https://twitter.com/*%E5%AF%BF%E5%8F%B8%E6%89%93
+
+class GameScore {
+  private url: string;
+
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  public function extractScore(url: string): void {
+    console.log(url.match(/★(\d,\d+)円/));
+  }  
+}
+
+chrome.webNavigation.onCompleted.addListener((details) => {
+  if (details.url.indexOf("https://twitter.com/") > -1) {
+    var url = decodeURIComponent(details.url);
+
+    var score = url.match(/★(\d,\d+)円/)[1];
+    var speed = url.match(/速度：(\d+.\d+)/)[1];
+    var miss = url.match(/ミス：(\d+)/)[1];
+    var course = url.match(/(\d,\d+)円コース/)[1];
+
+    console.log(details.tabId, score, speed, miss, course);
+
+    var key = details.tabId.toString();
+    var value = score;
+
+    // chrome.storage.local.clear();
+
+    // chrome.storage.local.set({ [key]: value }, () => {
+    //   console.log("Value is set");
+    // });
+
+    chrome.storage.local.get(null, (result) => {
+      console.log(result);
+    });
+  }
+});
+
+chrome.action.onClicked.addListener(() => {
+  // prettier-ignore
+  chrome.tabs.create({ "url": "src/index.html" });
+});
