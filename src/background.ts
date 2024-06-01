4bb074e20f04866e5ba72066166f55f912ca3e23
hrysdev <hrysdev@gmail.com>
2024-06-01 23:32:48 +0900

    wip
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/background.ts
+++ src/background.ts
@@ -1,22 +1,34 @@
+import { OverPayment } from "./overPayment";
+
+// chrome.storage.local.clear();
+
+function matchOrZero(input: string, regex: RegExp): number {
+  const matchResult = input.match(regex);
+  return matchResult ? Number(matchResult[1]) : 0;
+}
+
 chrome.webNavigation.onCompleted.addListener((details) => {
   if (details.url.indexOf("https://x.com/") > -1) {
     const url = decodeURIComponent(details.url).replaceAll(",", "");
 
-    const score = url.match(/★(\d+)円/)?.[1];
-    const speed = url.match(/速度：(\d+.\d+)/)?.[1];
-    const miss = url.match(/ミス：(\d+)/)?.[1];
-    const course = url.match(/(\d+)円コース/)?.[1];
+    const overPaymentRegex = /★(\d+)円/;
+    const overPayment = new OverPayment(matchOrZero(url, overPaymentRegex));
 
-    console.log(url);
-    console.log(details.tabId, score, speed, miss, course);
+    const speedRegex = /速度：(\d+.\d+)/;
+    const speed = matchOrZero(url, speedRegex);
+
+    // const miss = url.match(/ミス：(\d+)/)?.[1];
+    // const course = url.match(/(\d+)円コース/)?.[1];
+
+    const date = new Date();
+    console.log(date);
 
     const key = details.tabId.toString();
     const value = {
-      score: score,
+      score: overPayment.amount,
       speed: speed,
+      date: date,
     };
-
-    // chrome.storage.local.clear();
 
     chrome.storage.local.set({ [key]: value }, () => {
       console.log("Value is set");
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
 import Chart from "chart.js/auto";
 
+const dummyData = {
+  1: { score: "3000", speed: "4.1", date: "2024-01-01" },
+  2: { score: "3200", speed: "4.2", date: "2024-01-02" },
+  3: { score: "3500", speed: "4.3", date: "2024-01-03" },
+  4: { score: "3900", speed: "4.4", date: "2024-01-04" },
+  5: { score: "4400", speed: "4.5", date: "2024-01-05" },
+};
+
 type Item = {
   score: string;
   speed: string;
+  date: string;
 };
 
 chrome.storage.local.get(null, (result) => {
-  const keys: string[] = Object.keys(result);
-  const values: Item[] = Object.values(result);
+  console.log(result);
+  const values: Item[] = Object.values(dummyData);
 
   const ctx = document.getElementById("myChart") as HTMLCanvasElement;
   if (ctx) {
     new Chart(ctx, {
       type: "line",
       data: {
-        labels: keys,
+        labels: values.map((item) => item.date),
         datasets: [
           {
             label: "chart label",
