58caefa9cbbf72b7ba80937ed37d1a5720e75a84
hrysdev <hrysdev@gmail.com>
2024-06-01 15:52:31 +0900

    update: storageのデータをchart.jsによって、可視化できるように修正
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/content.ts
+++ src/content.ts
@@ -1,23 +1,28 @@
 import Chart from "chart.js/auto";
 
-const ctx = document.getElementById("myChart") as HTMLCanvasElement;
-
-if (ctx) {
-  new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: ["data1", "data2", "data3"],
-      datasets: [
-        {
-          label: "chart label",
-          data: [1, 2, 3],
-        },
-      ],
-    },
-  });
-}
-
 chrome.storage.local.get(null, (result) => {
   console.log(result);
-  chrome.runtime.sendMessage(result);
+
+  const keys: string[] = Object.keys(result);
+  console.log(keys);
+
+  const values: string[] = Object.values(result);
+  console.log(values);
+
+  const ctx = document.getElementById("myChart") as HTMLCanvasElement;
+
+  if (ctx) {
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: keys,
+        datasets: [
+          {
+            label: "chart label",
+            data: values,
+          },
+        ],
+      },
+    });
+  }
 });
