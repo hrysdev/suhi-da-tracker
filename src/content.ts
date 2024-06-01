a5a20c441a9be10cd848c8d3391b135cad823d49
hrysdev <hrysdev@gmail.com>
2024-06-01 15:55:12 +0900

    wip
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/content.ts
+++ src/content.ts
@@ -1,16 +1,10 @@
 import Chart from "chart.js/auto";
 
 chrome.storage.local.get(null, (result) => {
-  console.log(result);
-
   const keys: string[] = Object.keys(result);
-  console.log(keys);
-
   const values: string[] = Object.values(result);
-  console.log(values);
 
   const ctx = document.getElementById("myChart") as HTMLCanvasElement;
-
   if (ctx) {
     new Chart(ctx, {
       type: "line",
