5eca96031a2ee02fffd8840e0e088403bb874445
hrysdev <hrysdev@gmail.com>
2024-06-07 00:16:12 +0900

    initial commit
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Chart } from "chart.js/auto";
+
+const dummyData = {
+  1: {
+    typingCourse: "5000",
+    typingOverPayment: "3000",
+    typingRate: "4.1",
+    typingDate: "2024-01-01",
+  },
+  2: {
+    typingCourse: "5000",
+    typingOverPayment: "3200",
+    typingRate: "4.2",
+    typingDate: "2024-01-02",
+  },
+  3: {
+    typingCourse: "5000",
+    typingOverPayment: "3500",
+    typingRate: "4.3",
+    typingDate: "2024-01-03",
+  },
+  4: {
+    typingCourse: "5000",
+    typingOverPayment: "3900",
+    typingRate: "4.4",
+    typingDate: "2024-01-04",
+  },
+  5: {
+    typingCourse: "5000",
+    typingOverPayment: "4400",
+    typingRate: "4.5",
+    typingDate: "2024-01-05",
+  },
+  6: {
+    typingCourse: "5000",
+    typingOverPayment: "5000",
+    typingRate: "4.1",
+    typingDate: "2024-01-06",
+  },
+  7: {
+    typingCourse: "5000",
+    typingOverPayment: "10000",
+    typingRate: "4.2",
+    typingDate: "2024-01-07",
+  },
+};
+
+type TypingProp = {
+  typingCourse: string;
+  typingOverPayment: string;
+  typingRate: string;
+  typingDate: string;
+};
+
+chrome.storage.local.get(null, (result) => {
+  console.log(result);
+  const typingProp: TypingProp[] = Object.values(dummyData);
+
+  const ctx = document.getElementById("myChart") as HTMLCanvasElement;
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: typingProp.map((row) => row.typingDate),
+      datasets: [
+        {
+          label: "お勧め5,000円コース",
+          data: typingProp.map((row) => row.typingOverPayment),
+          borderColor: "rgb(75, 192, 192)",
+          backgroundColor: "rgb(75, 192, 192)",
+          tension: 0.2,
+        },
+      ],
+    },
+    options: {
+      scales: {},
+    },
+  });
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Sushi-Da Tracker</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
