26d8bfafe2297dd6d1fd9c554d451e5228867688
hrysdev <hrysdev@gmail.com>
2024-06-06 21:18:50 +0900

    feat: manifest.jsonを追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "Sushi-Da Tracker",
+  version: "0.0.1",
+  description: "寿司打のスコアを集約し、成長を可視化する Chrome の拡張機能",
+  background: {
+    service_worker: "src/background.tsx",
+    type: "module",
+  },
+  action: {},
+  permissions: ["webNavigation", "storage", "unlimitedStorage"],
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), crx({ manifest })],
+});
