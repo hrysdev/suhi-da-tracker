0591225868c2cd3daca29b8011b257ffd57fed46
hrysdev <hrysdev@gmail.com>
2024-06-11 00:47:10 +0900

    feat: スコアを抽出する機能を追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/background.tsx
+++ src/background.tsx
@@ -4,7 +4,9 @@ chrome.action.onClicked.addListener(() => {
 
 chrome.webNavigation.onCompleted.addListener((details) => {
   if (details.url.indexOf("https://x.com/") > -1) {
-    const xUrl = decodeURIComponent(details.url);
-    console.log(xUrl);
+    const xUrl = decodeURIComponent(details.url).replace(/,/g, "");
+
+    const overPayment = xUrl.match(/\d+/g)?.[1];
+    console.log(overPayment);
   }
 });
