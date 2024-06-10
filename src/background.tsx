36979cdfaccd12d02dac854ee328404c30bed437
hrysdev <hrysdev@gmail.com>
2024-06-11 00:21:05 +0900

    feat: url取得機能を追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/background.tsx
+++ src/background.tsx
@@ -1,3 +1,10 @@
 chrome.action.onClicked.addListener(() => {
-    chrome.tabs.create({ url: "../index.html" });
+  chrome.tabs.create({ url: "../index.html" });
+});
+
+chrome.webNavigation.onCompleted.addListener((details) => {
+  if (details.url.indexOf("https://x.com/") > -1) {
+    const xUrl = decodeURIComponent(details.url);
+    console.log(xUrl);
+  }
 });
