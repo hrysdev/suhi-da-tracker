5ff3a2f2331b0a62208053d1aa7437a1ea29fd55
hrysdev <hrysdev@gmail.com>
2024-06-20 23:31:17 +0900

    wip
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline } from "@mui/material";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <React.StrictMode>
     <CssBaseline />
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
