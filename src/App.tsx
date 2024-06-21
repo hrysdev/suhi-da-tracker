df3c16a1c043b20b628e4773d1ca18bc2096c26c
hrysdev <hrysdev@gmail.com>
2024-06-21 23:11:36 +0900

    feat: CSSベースラインを移動
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,16 @@
-import { LineChart, Line } from "recharts";
-import ButtonAppBar from "./components/ButtonAppBar";
-
-const dummyData = {
-  1: {
-    typingCourse: "5000",
-    typingOverPayment: "3000",
-    typingRate: "4.1",
-    typingDate: "2024-01-01",
-  },
-  2: {
-    typingCourse: "5000",
-    typingOverPayment: "3200",
-    typingRate: "4.2",
-    typingDate: "2024-01-02",
-  },
-  3: {
-    typingCourse: "5000",
-    typingOverPayment: "3500",
-    typingRate: "4.3",
-    typingDate: "2024-01-03",
-  },
-  4: {
-    typingCourse: "5000",
-    typingOverPayment: "3900",
-    typingRate: "4.4",
-    typingDate: "2024-01-04",
-  },
-  5: {
-    typingCourse: "5000",
-    typingOverPayment: "4400",
-    typingRate: "4.5",
-    typingDate: "2024-01-05",
-  },
-  6: {
-    typingCourse: "5000",
-    typingOverPayment: "5000",
-    typingRate: "4.1",
-    typingDate: "2024-01-06",
-  },
-  7: {
-    typingCourse: "5000",
-    typingOverPayment: "10000",
-    typingRate: "4.2",
-    typingDate: "2024-01-07",
-  },
-};
-
-type TypingProp = {
-  typingCourse: string;
-  typingOverPayment: string;
-  typingRate: string;
-  typingDate: string;
-};
+import CssBaseline from "@mui/material/CssBaseline";
+import OverPaymentLineChart from "./components/OverPaymentLineChart";
 
 // chrome.storage.local.get(null, (result) => {
 //   const typingProp: TypingProp[] = Object.values(dummyData);
 // });
 
-const typingProp: TypingProp[] = Object.values(dummyData);
-
 export default function App() {
   return (
     <>
-      <ButtonAppBar />
-      <LineChart width={400} height={400} data={typingProp}>
-        <Line type="monotone" dataKey="typingOverPayment" stroke="#8884d8" />
-      </LineChart>
-      <footer>© 2024 HrysDev.</footer>
+      <CssBaseline />
+      <OverPaymentLineChart />
+      {/* <footer>© 2024 HrysDev.</footer> */}
     </>
   );
 }
