3aaf82ce117ca6cad5c766b8b5941316ddbd2e60
hrysdev <hrysdev@gmail.com>
2024-06-24 00:46:53 +0900

    feat: Footerを追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import Typography from "@mui/material/Typography";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+
+export default function Footer() {
+  return (
+    <Stack direction="column" justifyContent="center" alignItems="center">
+      <IconButton href="https://github.com/hrysdev/sushi-da-tracker">
+        <GitHubIcon />
+      </IconButton>
+      <Typography>Copyright &copy; 2024 HrysDev.</Typography>
+    </Stack>
+  );
+}
