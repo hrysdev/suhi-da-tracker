5ad5a6dc6c0f83d4ec1ff8ca861ed664c2dac9a3
hrysdev <hrysdev@gmail.com>
2024-06-26 22:32:40 +0900

    feat: GitHubアイコンを削除
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,4 @@
 import Typography from "@mui/material/Typography";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import IconButton from "@mui/material/IconButton";
-import Stack from "@mui/material/Stack";
 
 export default function Footer() {
   return <Typography>Copyright &copy; 2024 HrysDev.</Typography>;
