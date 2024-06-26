a5d225509310a2988138929949c9860021c01911
hrysdev <hrysdev@gmail.com>
2024-06-26 21:16:49 +0900

    feat: ブラウザ設定によるテーマの切り替えを実装
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { ThemeProvider, createTheme } from "@mui/material";
+import React from "react";
+import { useMediaQuery, createTheme, ThemeProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import ScoreLineChart from "./components/ScoreLineChart";
 import Header from "./components/Header";
@@ -10,13 +11,17 @@ import Stack from "@mui/material/Stack";
 // });
 
 export default function App() {
-  const theme = createTheme({
-    palette: {
-      background: {
-        default: "#f5f5f5",
-      },
-    },
-  });
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
 
   return (
     <ThemeProvider theme={theme}>
