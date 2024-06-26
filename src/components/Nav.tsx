831fc6c1f0a985fca37742d4dc4c4dc0c6fb40ef
hrysdev <hrysdev@gmail.com>
2024-06-27 01:06:35 +0900

    feat: 見た目だけ仮実装
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,28 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import MenuIcon from "@mui/icons-material/Menu";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 export default function Nav() {
   return (
     <Box>
       <AppBar position="static">
-        <Toolbar sx={{ justifyContent: "space-between" }}>
-          <IconButton size="large">
+        <Toolbar>
+          <IconButton size="large" edge="start" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
+          <Typography variant="h5" sx={{ flexGrow: 1 }}>
+            寿司打トラッカー
+          </Typography>
           <IconButton size="large">
             <GitHubIcon />
+          </IconButton>
+          <IconButton size="large" edge="end">
+            <MoreVertIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
