0b46f0210bd4d130142372e5b0a4ec676190e198
hrysdev <hrysdev@gmail.com>
2024-07-02 21:05:46 +0900

    feat: スコアの保存機能を実装
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/background/background.tsx
+++ src/background/background.tsx
@@ -1,16 +1,43 @@
+import { storage } from "@extend-chrome/storage";
+
 chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({ url: "../index.html" });
+  chrome.tabs.create({ url: "../../index.html" });
 });
 
 chrome.webNavigation.onCompleted.addListener((details) => {
-  if (details.url.indexOf("https://x.com/") > -1) {
-    const xUrl = decodeURIComponent(details.url).replace(/,/g, "");
+  const { url, tabId } = details;
 
-    const values = xUrl.match(/\d+/g);
-    if (values) {
-      for (const value of values) {
-        console.log(value);
-      }
+  if (url.indexOf("https://x.com/") > -1) {
+    const decodeUrI = decodeURIComponent(url).replace(/,/g, "");
+    const level = decodeUrI.match(/【.+】/g)!;
+    const results = decodeUrI
+      .match(/\d+(\.\d+)?/g)!
+      .map((result) => parseFloat(result));
+
+    console.log(results);
+
+    if (decodeUrI.indexOf("損でした…") > -1) results[1] = -results[1];
+
+    if (results.length === 4) {
+      storage.local.set({
+        [tabId]: {
+          level: level[0],
+          course: results[0],
+          score: results[1],
+          rate: results[2],
+          miss: results[3],
+        },
+      });
+    } else {
+      storage.local.set({
+        [tabId]: {
+          level: level[0],
+          course: results[0],
+          score: results[3],
+          rate: results[4],
+          miss: results[5],
+        },
+      });
     }
   }
 });
