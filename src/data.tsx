e12b47dbb3723d14c079516d0f6a3582e27106d6
hrysdev <hrysdev@gmail.com>
2024-07-09 23:41:31 +0900

    feat: ui実装用のダミーデータを追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

---
+++ src/data.tsx
@@ -0,0 +1,87 @@
+// UI実装用の仮データ置き場。実データが取得できるまでの間に使う。
+export const data = {
+  1: {
+    course: 5000,
+    score: 3000,
+    rate: 4.1,
+    date: "24/01/01"
+  },
+  2: {
+    course: 3000,
+    score: 3200,
+    rate: 4.2,
+    date: "24/01/02"
+  },
+  3: {
+    course: 10000,
+    score: 3500,
+    rate: 4.3,
+    date: "24/01/03"
+  },
+  4: {
+    course: 5000,
+    score: 3900,
+    rate: 4.4,
+    date: "24/01/04"
+  },
+  5: {
+    course: 3000,
+    score: 4400,
+    rate: 4.5,
+    date: "24/01/05"
+  },
+  6: {
+    course: 10000,
+    score: 5000,
+    rate: 4.1,
+    date: "24/01/06"
+  },
+  7: {
+    course: 3000,
+    score: 4000,
+    rate: 4.2,
+    date: "24/01/07"
+  },
+  8: {
+    course: 10000,
+    score: 4200,
+    rate: 4.3,
+    date: "24/01/08"
+  },
+  9: {
+    course: 5000,
+    score: 4500,
+    rate: 4.4,
+    date: "24/01/09"
+  },
+  10: {
+    course: 5000,
+    score: 4900,
+    rate: 4.5,
+    date: "24/01/10"
+  },
+  11: {
+    course: 3000,
+    score: 5500,
+    rate: 4.1,
+    date: "24/01/11"
+  },
+  12: {
+    course: 5000,
+    score: 6000,
+    rate: 4.2,
+    date: "24/01/12"
+  },
+  13: {
+    course: 10000,
+    score: 7000,
+    rate: 4.3,
+    date: "24/01/13"
+  },
+  14: {
+    course: 5000,
+    score: 8000,
+    rate: 4.4,
+    date: "24/01/14"
+  }
+}
