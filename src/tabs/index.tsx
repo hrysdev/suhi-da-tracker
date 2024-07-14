e0db09890187b3d2479de18551d7527a4ca85c26
hrysdev <hrysdev@gmail.com>
2024-07-15 04:17:57 +0900

    feat: 仮UIを実装
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/tabs/index.tsx
+++ src/tabs/index.tsx
@@ -1,9 +1,12 @@
-import ScoreCard from "@components/ScoreCard"
+import MaxMissCard from "@components/MaxMissCard"
+import MaxRateCard from "@components/MaxRateCard"
+import MaxScoreCard from "@components/MaxScoreCard"
 import ScoreChart from "@components/ScoreChart"
 import Button from "@mui/material/Button"
 import CssBaseline from "@mui/material/CssBaseline"
 import Grid from "@mui/material/Grid"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
+import Typography from "@mui/material/Typography"
 import { StrictMode } from "react"
 
 export default function TabPage() {
@@ -13,20 +16,23 @@ export default function TabPage() {
     <StrictMode>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Grid container spacing={3} sx={{ px: 11 }}>
+        <Grid container spacing={3} sx={{ px: 8, mb: 4 }}>
           <Grid item xs={4}>
-            <ScoreCard />
+            <MaxScoreCard />
           </Grid>
           <Grid item xs={4}>
-            <ScoreCard />
+            <MaxRateCard />
           </Grid>
           <Grid item xs={4}>
-            <ScoreCard />
+            <MaxMissCard />
           </Grid>
           <Grid item xs={12}>
             <ScoreChart />
           </Grid>
         </Grid>
+        <Typography textAlign="center">
+          Copyright &copy; 2024 HrysDev.
+        </Typography>
         <Button variant="outlined">何故かこれがないと動かない</Button>
       </ThemeProvider>
     </StrictMode>
