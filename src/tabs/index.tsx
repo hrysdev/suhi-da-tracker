7c60c6d7585c9f8723a0dfda75c43bb7ccf8c1a3
hrysdev <hrysdev@gmail.com>
2024-07-16 02:53:43 +0900

    feat: AppBarを追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/tabs/index.tsx
+++ src/tabs/index.tsx
@@ -1,11 +1,16 @@
+// import MaxMissCard from "@components/MaxMissCard"
+// import MaxRateCard from "@components/MaxRateCard"
+import DailyMaxCostChart from "@components/DailyMaxCostChart"
 import MaxCostCard from "@components/MaxCostCard"
-import MaxMissCard from "@components/MaxMissCard"
-import MaxRateCard from "@components/MaxRateCard"
-import ScoreChart from "@components/ScoreChart"
+import GitHubIcon from "@mui/icons-material/GitHub"
+import MoreVertIcon from "@mui/icons-material/MoreVert"
+import AppBar from "@mui/material/AppBar"
 import Button from "@mui/material/Button"
 import CssBaseline from "@mui/material/CssBaseline"
 import Grid from "@mui/material/Grid"
+import IconButton from "@mui/material/IconButton"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
+import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
 import { StrictMode } from "react"
 
@@ -16,22 +21,36 @@ export default function TabPage() {
     <StrictMode>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Grid container spacing={3} sx={{ px: 8, mb: 4 }}>
+        <AppBar
+          color="inherit"
+          position="static"
+          elevation={0}
+          sx={{ alignItems: "end" }}>
+          <Toolbar>
+            <IconButton>
+              <GitHubIcon />
+            </IconButton>
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Grid container spacing={3} sx={{ px: 11, mb: 4 }}>
           <Grid item xs={4}>
             <MaxCostCard />
           </Grid>
-          <Grid item xs={4}>
+          {/* <Grid item xs={4}>
             <MaxRateCard />
           </Grid>
           <Grid item xs={4}>
             <MaxMissCard />
-          </Grid>
+          </Grid> */}
           <Grid item xs={12}>
-            <ScoreChart />
+            <DailyMaxCostChart />
           </Grid>
         </Grid>
         <Typography textAlign="center">
-          Copyright &copy; 2024 HrysDev.
+          &copy; 2024 hrysdev@gmail.com
         </Typography>
         <Button variant="outlined">何故かこれがないと動かない</Button>
       </ThemeProvider>
