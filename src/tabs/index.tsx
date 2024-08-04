10544dab91b89bd5fc342f80507e3fc95f0d9512
hrysdev <hrysdev@gmail.com>
2024-08-05 02:08:32 +0900

    feat: chipボタンを削除
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/tabs/index.tsx
+++ src/tabs/index.tsx
@@ -3,12 +3,10 @@ import ResultLogTable from "@components/ResultLogTable"
 import GitHubIcon from "@mui/icons-material/GitHub"
 import AppBar from "@mui/material/AppBar"
 import Button from "@mui/material/Button"
-import Chip from "@mui/material/Chip"
 import Container from "@mui/material/Container"
 import CssBaseline from "@mui/material/CssBaseline"
 import Grid from "@mui/material/Grid"
 import IconButton from "@mui/material/IconButton"
-import Stack from "@mui/material/Stack"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
@@ -17,38 +15,24 @@ import { StrictMode } from "react"
 export default function TabPage() {
   const theme = createTheme()
 
-  const handleClick = () => {
-    console.log("Chip Clicked")
-  }
-
   return (
     <StrictMode>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <AppBar color="transparent" position="static" elevation={0}>
-          <Toolbar sx={{ justifyContent: "space-between" }}>
-            <Typography variant="h5">寿司打日誌（ver.0.0.1）</Typography>
-            <IconButton
-              size="large"
-              edge="end"
-              color="inherit"
-              href="https://github.com/hrysdev/suhi-da-tracker"
-              target="_blank">
-              <GitHubIcon fontSize="large" />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        {/* <Stack direction="row" spacing={2}>
-          <Chip
-            label="3,000円コース"
-            color="primary"
-            onClick={handleClick}
-            clickable
-          />
-          <Chip label="5,000円コース" color="primary" clickable />
-          <Chip label="10,000円コース" color="primary" clickable />
-        </Stack> */}
-        <Container maxWidth="md" sx={{ mb: 4 }}>
+        <Container maxWidth="lg" sx={{ mb: 4 }}>
+          <AppBar color="transparent" position="static" elevation={0}>
+            <Toolbar sx={{ justifyContent: "space-between" }}>
+              <Typography variant="h5">寿司打日誌（ver.0.0.1）</Typography>
+              <IconButton
+                size="large"
+                edge="end"
+                color="inherit"
+                href="https://github.com/hrysdev/suhi-da-tracker"
+                target="_blank">
+                <GitHubIcon fontSize="large" />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
           <Grid container spacing={3}>
             <Grid item xs={12}>
               <DailyMaxScoreChart />
